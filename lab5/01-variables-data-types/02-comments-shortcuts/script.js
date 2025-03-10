d1dd586a5e1c5a95bad20bf749d8018b9e192e5e
Warissara67 <khim0931804748@gmail.com>
2025-03-10 12:27:12 +0700

    add all-file

---
+++ lab5/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,26 @@
+console.log("Hello Word.");
+console.log(2+9);
+console.log("answer:"+23.96+7)
+
+const cat ="Cherry";
+let dog;
+/*
+dog=("joey");
+console.log(cat);
+console.log(dog);
+*/
+
+/*
+// Log nomber
+console.log(100);
+*/
+
+/*
+// Log String
+console.log('Hello Word');
+*/
+
+/*
+// Log multiple values
+console.log(48)
+*/
---
+++ lab5/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,67 @@
+let name;
+let surname;
+
+let surName;
+let cat;
+
+let cats=["cherry","Joss"];
+
+let schoolname;
+let school_name;
+let SchoolName;
+let my_school_name;
+
+let my_name;
+
+my_name = "Warissara"; // Assigment statement
+
+console.log(my_name);
+
+let a=1;
+let b=2;
+let c=1.3;
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+
+let bigNumber=100000000000000n;
+
+console.log(bigNumber, typeof bigNumber);
+
+console.log(cats, typeof cats);
+
+console.log(my_name, typeof my_name);
+let is_deleted = false;
+
+console.log(is_deleted, typeof is_deleted);
+
+a=1;
+console.log(a, typeof a);
+
+a="Test"; //re-assign to string value
+console.log(a, typeof a);
+a=a+2;
+
+b=2;
+console.log(b, typeof b);
+
+// We can still manipulate arrys and oject
+const arr = [1,2,3,4,];
+console.log(arr); 
+
+arr.push(5);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+const person = {
+    name: 'Brad',
+    email: 'brad@demo.com'
+};
+console.log(person);
+
+person.name = 'John';
+person.email = 'john@email.com'
+console.log(person);
